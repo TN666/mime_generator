ada929e2c25fc71d5cb60be605b12381846984ce
Ting <tingli@softnext.com.tw>
2023-05-02 21:01:07 +0800

    start

--- src/App.js
+++ src/App.js
@@ -1,23 +1,42 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [text, setText] = useState('');
+  const [imageUrl, setImageUrl] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+  const OPENAI_API_KEY = '${YOUR_OPENAI_API_KEY}';
+
+  const handlePostRequest = async () => {
+    setIsLoading(true);
+    setImageUrl(null);
+    // Send POST request to endpoint
+    const response = await fetch('https://api.openai.com/v1/images/generations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Authorization': `Bearer ${OPENAI_API_KEY}`
+      },
+      body: JSON.stringify({
+        'prompt': `${text} Meme`,
+        'n': 1,
+        'size': '512x512'
+      })
+    });
+    const data = await response.json();
+    // Update state with image URL
+    setImageUrl(data.data[0].url);
+    setIsLoading(false);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Meme Generator</h1>
+      <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter a keyword..." />
+      <button type="submit" onClick={handlePostRequest}>{isLoading ? 'Sending Data...' : 'Send Data'}</button>
+      <div className="image-container">
+        <img src={imageUrl} alt="" />
+      </div>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,73 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, fireEvent, screen, waitFor } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component', () => {
+  it('should render the input field, button, and image container', () => {
+    render(<App />);
+    expect(screen.getByPlaceholderText('Enter a keyword...')).toBeInTheDocument();
+    expect(screen.getByRole('button', { name: 'Send Data' })).toBeInTheDocument();
+    expect(screen.getByAltText('')).toBeInTheDocument();
+  });
+
+  it('should update the input field value when the user enters text', () => {
+    render(<App />);
+    const input = screen.getByPlaceholderText('Enter a keyword...');
+    fireEvent.change(input, { target: { value: 'dog' } });
+    expect(input.value).toBe('dog');
+  });
+
+  it('should fetch data and display an image when the user clicks the button', async () => {
+    // Mock fetch API
+    const mockFetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            data: [
+              {
+                url: 'https://example.com/image.png',
+              },
+            ],
+          }),
+      })
+    );
+    global.fetch = mockFetch;
+
+    render(<App />);
+    const input = screen.getByPlaceholderText('Enter a keyword...');
+    const button = screen.getByRole('button', { name: 'Send Data' });
+    fireEvent.change(input, { target: { value: 'cat' } });
+    fireEvent.click(button);
+
+    // Wait for the image to load
+    await waitFor(() => {
+      expect(screen.getByAltText('')).toHaveAttribute('src', 'https://example.com/image.png');
+    });
+
+    expect(mockFetch).toHaveBeenCalledTimes(1);
+    expect(mockFetch).toHaveBeenCalledWith('https://api.openai.com/v1/images/generations', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer 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',
+      },
+      body: JSON.stringify({
+        prompt: 'cat Meme',
+        n: 1,
+        size: '512x512',
+      }),
+    });
+  });
+
+  it('should show a loading message when data is being sent', async () => {
+    // Mock fetch API
+    const mockFetch = jest.fn(() => Promise.resolve({ json: () => Promise.resolve({ data: [{ url: 'image.png' }] }) }));
+    global.fetch = mockFetch;
+
+    render(<App />);
+    const button = screen.getByRole('button', { name: 'Send Data' });
+    fireEvent.click(button);
+
+    expect(screen.getByText('Sending Data...')).toBeInTheDocument();
+  });
 });
